f7d44c91478d39bdde19d3be2901e16ce3538132
Jan Chlouba <cboubik@gmail.com>
2024-04-16 10:37:35 +0200

    defaul config for mail

---
+++ config.py
@@ -0,0 +1,7 @@
+def get_config():
+    return {
+        "server": '127.0.0.1',  # Replace with your e-mail server's address
+        "port": 587,  # Typically 587 for TLS
+        "username": 'email@example.con',  # Your full email address
+        "password": 'password'  # Your email account password
+    }
